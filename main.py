763edd4e33c35f480f4ec03b08f34264c5153535
Daeyeol Chang <79178854+dy-chang@users.noreply.github.com>
2024-02-28 12:45:05 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit
+
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+modelTraining = st.beta_container()
+
+with header:
+	st.title('Welcome to my project!')
